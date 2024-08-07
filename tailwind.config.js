5135ecc8dd36e7b2d5342d25ef45ae0d6e8f9a53
Arjun AV <avarjun360@gmail.com>
2024-08-07 12:13:33 +0530

    Updated the template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+    content: ["./src/**/*.{html,js}","./public/*.html","./node_modules/flowbite/**/*.js"],
     theme: {
         screens: {
             'tablet': '640px',
